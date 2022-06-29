af4721ee598e96879aa5d7dfe3fd0398898a39a4
PatrykKobylarczyk <patryk.kobylarczyk@gmail.com>
2022-06-29 02:34:42 +0200

    add portfolio and rwd

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 //active element in navigation
 
 const navList = document.querySelectorAll('.nav-list');
-const navListPortfolio = document.querySelectorAll('.nav-list-portfolio');
-console.log(navList); 
-                               
+                             
 //nav list
 navList.forEach(el => {
     el.addEventListener('click', () => {
