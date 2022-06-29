d11cd50ca9a901c54ec02e0a97cfb1c8c3a46f4e
PatrykKobylarczyk <patryk.kobylarczyk@gmail.com>
2022-06-29 18:23:58 +0200

    add simple gsap animations to index.html

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ navList.forEach(el => {
     });
 });
 
+//animations GSAP
+gsap.registerPlugin(ScrollTrigger);
+
+const titles = document.querySelector('.main-section-title') 
+const posters = document.querySelector('.main-section-posters')
+const texts = document.querySelector('.main-section-text')
+const works = document.querySelector('.main-section-works')
+
+
+const tl1 = gsap.timeline({defaults: { ease: 'slowMo.easeOut'}});
+const tl2 = gsap.timeline({defaults: { ease: 'slowMo.easeOut'}, scrollTrigger: {trigger: '.main-section-text', start: 'top 50%'}});
+const tl3 = gsap.timeline({defaults: { ease: 'slowMo.easeOut'}, scrollTrigger: {trigger: '.main-section-works', start: 'top 50%'}});
+
+tl1.fromTo(titles.children, {y: '+=50', opacity: 0 }, {y: '0', opacity: 1, stagger: .3, duration: .7})
+  .fromTo(posters.children, {y: '+=50', opacity: 0 }, {y: '0', opacity: 1, stagger: .3, duration: .7});
+
+tl2.fromTo(texts.children, 
+    {y: '+=50', opacity: 0 }, 
+    {y: '0', opacity: 1, stagger: .3, duration: .7, });
+
+tl3.fromTo(works.children, 
+    {y: '+=50', opacity: 0 }, 
+    {y: '0', opacity: 1, stagger: .5, duration: .7});
+
